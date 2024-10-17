cb5536b4236e5a1916b8d750bddccfd706bd32b7
IZeck20 <camilogamer105138@gmail.com>
2024-10-17 12:23:59 -0500

    modelos aves

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import { useState } from 'react';
 import './App.css';
 
 const modelos = [
-  {
-    src: "https://cdn.glitch.global/ba14295b-4cfc-4132-aa30-9f73c1da8256/Dayo.glb?v=1729178257700",
-    marker: "https://cdn.glitch.global/ba14295b-4cfc-4132-aa30-9f73c1da8256/foto-deliciosa-hamburguesa-aislado-sobre-fondo-blanco.jpg?v=1729182079387" // Reemplaza con la URL de tu primer marcador
-  }
+  "https://cdn.glitch.global/ba14295b-4cfc-4132-aa30-9f73c1da8256/Dayo.glb?v=1729178257700",
+  "https://cdn.glitch.global/ba14295b-4cfc-4132-aa30-9f73c1da8256/Petirrojo.glb?v=1729185728694",
+  "https://cdn.glitch.global/ba14295b-4cfc-4132-aa30-9f73c1da8256/Bichofue.glb?v=1729185739253"
 ];
 
 function App() {
@@ -19,20 +18,21 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Visor de Modelos 3D por Marcadores</h1>
-      <button onClick={() => cambiarModelo(-1)}>Anterior</button>
-      <button onClick={() => cambiarModelo(1)}>Siguiente</button>
+      <h1>Visor de Modelos 3D</h1>
+      
+      <model-viewer
+        src={modelos[currentModelIndex]}
+        alt="Modelo 3D"
+        auto-rotate
+        camera-controls
+        ar
+        style={{ width: '100%', height: '500px' }}
+      ></model-viewer>
 
-      <a-scene embedded arjs="sourceType: webcam;">
-        <a-marker url={modelos[currentModelIndex].marker}>
-          <a-entity
-            gltf-model={modelos[currentModelIndex].src}
-            scale="0.1 0.1 0.1" // Ajusta el tamaño según sea necesario
-            position="0 0 0"
-          ></a-entity>
-        </a-marker>
-        <a-entity camera></a-entity>
-      </a-scene>
+      <div className="botones">
+        <button onClick={() => cambiarModelo(-1)}>Anterior</button>
+        <button onClick={() => cambiarModelo(1)}>Siguiente</button>
+      </div>
     </div>
   );
 }
